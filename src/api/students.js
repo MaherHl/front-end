332081a846cfbaba6d98f0aa9cacfeafcf8c0763
hamza mouddakir <hamzamouddakur@gmail.com>
2022-03-26 15:49:23 +0100

    add files json

---
+++ src/api/students.js
@@ -0,0 +1,325 @@
+let Students = [
+    {
+      "username": "mojombo",
+      "id": 1,
+      "avatar": "https://avatars.githubusercontent.com/u/1?v=4",
+      "full_name": "mahjob reda",
+      "email":"mojombo@gmail.com",
+      "github": "https://github.com/mojombo",
+      "description": "The App Framework for Startups",
+      "phone": "+212637916306"
+    },
+    {
+      "username": "defunkt",
+      "id": 2,
+      "avatar": "https://avatars.githubusercontent.com/u/2?v=4",
+      "full_name": "def unket",
+      "email":"defunkt@gmail.com",
+      "description": "Redwood Tutorial QA Week related items",
+      "phone": "+212637916306"
+    },
+    {
+      "username": "pjhyett",
+      "id": 3,
+      "email":"hamzamoro@gmail.com",
+      "avatar": "https://avatars.githubusercontent.com/u/3?v=4",
+      "full_name": "hamza tech",
+      "github": "https://github.com/pjhyett",
+      "description": "Redwood Tutorial QA Week related items",
+      "phone": "+212637916306"
+    },
+    {
+      "username": "wycats",
+      "id": 4,
+      "email":"ahmed@gmail.com",
+      "avatar": "https://avatars.githubusercontent.com/u/4?v=4",
+      "full_name": "way the recket",
+      "github": "https://github.com/wycats","description": "Identify critical items to consider for prioritization and patch release. For Process: https://github.com/redwoodjs/redwood/issues/2067",
+      "phone": "+212637916306"
+    },
+    {
+      "username": "ezmobius",
+      "id": 5,
+      "avatar": "https://avatars.githubusercontent.com/u/5?v=4",
+      "full_name": "hassan red",
+      "email":"mojombo@gmail.com",
+      "github": "https://github.com/ezmobius","description": "The App Framework for Startups",
+      "phone": "+212637916306"
+    },
+    {
+      "username": "ivey",
+      "id": 6,
+      "email":"ivey@gmail.com",
+      "avatar": "https://avatars.githubusercontent.com/u/6?v=4",
+      "full_name": "ivey rojer",
+      "url_github": "https://api.github.com/users/ivey",
+      "github": "https://github.com/ivey","description": "PRs must include one \"release:*\" label for categorization",
+      "phone": "+212637916306"
+    },
+    {
+      "username": "evanphx",
+      "id": 7,
+      "email":"evan@gmail.com",
+      "avatar": "https://avatars.githubusercontent.com/u/7?v=4",
+      "full_name": "even code",
+      "github": "https://github.com/evanphx",
+      "description": "The App Framework for Startups",
+      "phone": "+212637916306"
+    },
+    {
+      "username": "vanpelt",
+      "id": 17,
+      "email":"vanden@gmail.com",
+      "avatar": "https://avatars.githubusercontent.com/u/17?v=4",
+      "full_name": "van red",
+      "github": "https://github.com/vanpelt",
+      "description": "The App Framework for Startups",
+      "phone": "+212637916306"
+    },
+    {
+      "username": "wayneeseguin",
+      "id": 18,
+      "email":"mahmodredo@gmail.com",
+      "avatar": "https://avatars.githubusercontent.com/u/18?v=4",
+      "full_name": "mahmod redo",
+      "github": "https://github.com/wayneeseguin",
+      "description": "The App Framework for Startups",
+      "phone": "+212637916306"
+    },
+    {
+      "username": "brynary",
+      "id": 19,
+      "avatar": "https://avatars.githubusercontent.com/u/19?v=4",
+      "full_name": "binary binary",
+      "email":"binary@gmail.com",
+      "github": "https://github.com/brynary",
+      "description": "The App Framework for Startups",
+      "phone": "+212637916306"
+    },
+    {
+      "username": "kevinclark",
+      "id": 20,
+      "avatar": "https://avatars.githubusercontent.com/u/20?v=4",
+      "full_name": "keven said",
+      "email":"kevensaid@gmail.com",
+      "github": "https://github.com/kevinclark",
+      "description": "The App Framework for Startups",
+      "phone": "+212637916306"
+    },
+    {
+      "username": "technoweenie",
+      "id": 21,
+      "avatar": "https://avatars.githubusercontent.com/u/21?v=4",
+      "full_name": "tech wonie",
+      "email":"techwonnie@gmail.com",
+      "github": "https://github.com/technoweenie",
+      "description": "Redwood Tutorial QA Week related items",
+      "phone": "+212637916306"
+    },
+    {
+      "username": "macournoyer",
+      "id": 22,
+      "avatar": "https://avatars.githubusercontent.com/u/22?v=4",
+      "full_name": "ayoub mackour",
+      "email":"maco@gmail.com",
+      "github": "https://github.com/macournoyer",
+      "description": "The App Framework for Startups",
+      "phone": "+212637916306"
+    },
+    {
+      "username": "takeo",
+      "id": 23,
+      "avatar": "https://avatars.githubusercontent.com/u/23?v=4",
+      "full_name": "take care",
+      "email":"takeo@gmail.com",
+      "github": "https://github.com/takeo",
+      "description": "The App Framework for Startups",
+      "phone": "+212637916306"
+    },
+    {
+      "username": "caged",
+      "id": 25,
+      "avatar": "https://avatars.githubusercontent.com/u/25?v=4",
+      "full_name": " hamid miro",
+      "email":"hamidmiro@gmail.com",
+      "github": "https://github.com/caged",
+      "description": "The App Framework for Startups",
+      "phone": "+212637916306"
+    },
+    {
+      "username": "topfunky",
+      "id": 26,
+      "email":"ilyasnoufal@gmail.com",
+      "avatar": "https://avatars.githubusercontent.com/u/26?v=4",
+      "full_name": "ilyas noufal",
+      "url_github": "https://api.github.com/users/topfunky",
+      "github": "https://github.com/topfunky",
+      "description": "The App Framework for Startups",
+      "phone": "+212637916306"
+    },
+    
+    {
+      "username": "kevwil",
+      "id":27,
+      "bio": "amal mona developpeur front end i can make beautifull design and convert any screen to code",
+      "avatar": "https://avatars.githubusercontent.com/u/35?v=4",
+      "full_name": "amal mona",
+      "email":"amalmona@gmail.com",
+      "github": "https://github.com/kevwil",
+      "description": "The App Framework for Startups",
+      "phone": "+217916306"
+    },
+    {
+      "username": "KirinDave",
+      "id": 28,
+      "email":"zineb@gmail.com",
+      "avatar": "https://avatars.githubusercontent.com/u/36?v=4",
+      "full_name": "zineb said",
+      "github": "https://github.com/KirinDave",
+      "description": "The App Framework for Startups",
+      "phone": "+215647916306"
+    },
+    {
+      "username": "jamesgolick",
+      "id": 29,
+      "email":"jhondari@gmail.com",
+      "avatar": "https://avatars.githubusercontent.com/u/37?v=4",
+      "full_name": "jhon dari",
+      "url_github": "https://api.github.com/users/jamesgolick",
+      "github": "https://github.com/jamesgolick",
+      "description": "The App Framework for Startups",
+      "phone": "+21245916306"
+    },
+    {
+      "username": "atmos",
+      "id": 30,
+      "bio": "developpeur application mobile front end and backend ",
+      "avatar": "https://avatars.githubusercontent.com/u/38?v=4",
+      "full_name": "mos maher",
+      "email":"mojombo@gmail.com",
+      "github": "https://github.com/atmos",
+      "description": "The App Framework for Startups",
+      "phone": "+212456916306"
+    }]
+let jobs=[
+    {
+        id:1,
+        jobname:"template login",
+        datesatart:"12/11/2021",
+        datefin:"15/11/2021",
+        duration:"3 Days",
+        price:"15 $",
+        technologie:['html',"css","jquery"],
+        info:"template login using html and css with jquery the template should with script of validation of inforrmations"
+    },
+    {
+        id:2,
+        jobname:"sreen sign up",
+        datesatart:"12/10/2022",
+        datefin:"16/10/2022",
+        duration:"4 Days",
+        price:"50 $",
+        technologie:['adobe xd',"svg","illustrator"],
+        info:"screen of sign up the screen shoud hold 3 color blue , white , black  "
+    },
+    {
+        id:3,
+        jobname:"design application",
+        datesatart:"01/05/2022",
+        datefin:"01/07/2022",
+        duration:"2 moins",
+        price:"negotiable",
+        technologie:['kotlin',"json","figma","java"],
+        info:"design for application mobile the purpose of application is for chatting contact us for information"
+    },
+    {
+        id:4,
+        jobname:"porfolio",
+        datesatart:"12/10/2021",
+        datefin:"12/11/2021",
+        duration:"30 Days",
+        price:"150 $",
+        technologie:["you can choose the technologie "],
+        info:"template login using html and css with jquery the template should with script of validation of inforrmations"
+    },
+    {
+        id:5,
+        jobname:"writing bloggers",
+        datesatart:"01/01/2021",
+        datefin:"01/02/2022",
+        duration:"12 month",
+        price:"350$/month ",
+        technologie:['francias',"anglais","arabic"],
+        info:"write blogs in multiple domaine just you have to know three language can you write with high perfomance"
+    },
+    {
+        
+        id:6,
+        jobname:"template login",
+        datesatart:"12/11/2021",
+        datefin:"15/11/2021",
+        duration:"3 Days",
+        price:"15 $",
+        technologie:['html',"css","jquery"],
+        info:"template login using html and css with jquery the template should with script of validation of inforrmations"
+    }, 
+    {
+        
+        id:7,
+        jobname:"vedio animation",
+        datesatart:"12/11/2021",
+        datefin:"15/11/2021",
+        duration:"13 Days",
+        price:"10 $",
+        technologie:['filmora',"photoshop","english"],
+        info:"vedio animatio duration of vedio is 60 second "
+    }, 
+    {
+        
+        id:8,
+        jobname:"phtoshop pictures",
+        datesatart:"12/11/2021",
+        datefin:"20/11/2021",
+        duration:"8 Days",
+        price:"30 $",
+        technologie:['html',"css","jquery"],
+        info:"photoshop of several picture 15 picture should make change in background and color for more information contact us"
+    }, 
+]
+let Company = [
+  {
+    id:1,
+    name:'aserti',
+    avatar:"",
+    email:"asert@email.com",
+    description:"La société ASERTI est un acteur incontournable sur les marchés Composée d'une équipe spécialisée dans l'industrie agro-alimentaire, ASERTI propose à ses clients la conception,"
+  },
+  {
+    id:2,
+    name:'Atlas',
+    avatar:"",
+    email:"atlasmaroc@email.com",
+    description:"ATLAS est un acteur incontournable sur les marchés Composée d'une équipe spécialisée dans l'industrie agro-alimentaire, ASERTI propose à ses clients la conception,"
+  },
+  {
+    id:3,
+    name:'Bim',
+    avatar:"",
+    email:"bim@aswak.com",
+    description:"Bim est un entreprise qui vend plusieurs types des produits eletronice informatique nouritaure..."
+  },
+  {
+    id:4,
+    name:'asima',
+    avatar:"",
+    email:"micrpspft@email.com",
+    description:"asimsa ou aswak salam est un entreprise qui spesiale en vendre des produits ,"
+  },
+  {
+    id:5,
+    name:'netplus',
+    avatar:"",
+    email:"asert@email.com",
+    description:"La société NETPLUS est soiete de security et d'analyses les sites web la societe a un equipe spécialisée dans l'infrmatique la societe propose à ses clients de proteeger son site avec une grand perfomance"
+  }
+]
