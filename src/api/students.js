dcfa542607309694ae96dfdbc80ba17a80748fac
hamza mouddakir <hamzamouddakur@gmail.com>
2022-03-26 15:58:43 +0100

    add files json

--- src/api/students.js
+++ src/api/students.js
@@ -1,4 +1,4 @@
-let Students = [
+ let Students = [
     {
       "username": "mojombo",
       "id": 1,
@@ -34,7 +34,7 @@ let Students = [
       "email":"ahmed@gmail.com",
       "avatar": "https://avatars.githubusercontent.com/u/4?v=4",
       "full_name": "way the recket",
-      "github": "https://github.com/wycats","description": "Identify critical items to consider for prioritization and patch release. For Process: https://github.com/redwoodjs/redwood/issues/2067",
+      "github": "https://github.com/wycats","description": "identify critical items to consider for prioritization and patch release. For Process: https://github.com/redwoodjs/redwood/issues/2067",
       "phone": "+212637916306"
     },
     {
@@ -200,126 +200,134 @@ let Students = [
       "github": "https://github.com/atmos",
       "description": "The App Framework for Startups",
       "phone": "+212456916306"
-    }]
+    }];
+    
 let jobs=[
     {
-        id:1,
-        jobname:"template login",
-        datesatart:"12/11/2021",
-        datefin:"15/11/2021",
-        duration:"3 Days",
-        price:"15 $",
-        technologie:['html',"css","jquery"],
-        info:"template login using html and css with jquery the template should with script of validation of inforrmations"
+        "id":1,
+        "job_name":"template login",
+        "date_start":"12/11/2021",
+        "date_end":"15/11/2021",
+        "duration":"3 Days",
+        "price":"15 $",
+        "technologie":["html","css","jquery"],
+        "info":"template login using html and css with jquery the template should with script of validation of inforrmations"
     },
     {
-        id:2,
-        jobname:"sreen sign up",
-        datesatart:"12/10/2022",
-        datefin:"16/10/2022",
-        duration:"4 Days",
-        price:"50 $",
-        technologie:['adobe xd',"svg","illustrator"],
-        info:"screen of sign up the screen shoud hold 3 color blue , white , black  "
+        "id":2,
+        "job_name":"sreen sign up",
+        "date_start":"12/10/2022",
+        "date_end":"16/10/2022",
+        "duration":"4 Days",
+        "price":"50 $",
+        "technologie":["adobe xd","svg","illustrator"],
+        "info":"screen of sign up the screen shoud hold 3 color blue , white , black  "
     },
     {
-        id:3,
-        jobname:"design application",
-        datesatart:"01/05/2022",
-        datefin:"01/07/2022",
-        duration:"2 moins",
-        price:"negotiable",
-        technologie:['kotlin',"json","figma","java"],
-        info:"design for application mobile the purpose of application is for chatting contact us for information"
+        "id":3,
+        "job_name":"design application",
+        "date_start":"01/05/2022",
+        "date_end":"01/07/2022",
+        "duration":"2 moins",
+        "price":"negotiable",
+        "technologie":["kotlin","json","figma","java"],
+        "info":"design for application mobile the purpose of application is for chatting contact us for informations"
     },
     {
-        id:4,
-        jobname:"porfolio",
-        datesatart:"12/10/2021",
-        datefin:"12/11/2021",
-        duration:"30 Days",
-        price:"150 $",
-        technologie:["you can choose the technologie "],
-        info:"template login using html and css with jquery the template should with script of validation of inforrmations"
+        "id":4,
+        "job_name":"porfolio",
+        "date_start":"12/10/2021",
+        "date_end":"12/11/2021",
+        "duration":"30 Days",
+        "price":"150 $",
+        "technologie":["you can choose the technologie" ],
+        "info":"template login using html and css with jquery the template should with script of validation of informations"
     },
     {
-        id:5,
-        jobname:"writing bloggers",
-        datesatart:"01/01/2021",
-        datefin:"01/02/2022",
-        duration:"12 month",
-        price:"350$/month ",
-        technologie:['francias',"anglais","arabic"],
-        info:"write blogs in multiple domaine just you have to know three language can you write with high perfomance"
+        "id":5,
+        "job_name":"writing bloggers",
+        "date_start":"01/01/2021",
+        "date_end":"01/02/2022",
+        "duration":"12 month",
+        "price":"350$/month ",
+        "technologie":["francias","anglais","arabic"],
+        "info":"write blogs in multiple domaine just you have to know three language can you write with high perfomance"
     },
     {
         
-        id:6,
-        jobname:"template login",
-        datesatart:"12/11/2021",
-        datefin:"15/11/2021",
-        duration:"3 Days",
-        price:"15 $",
-        technologie:['html',"css","jquery"],
-        info:"template login using html and css with jquery the template should with script of validation of inforrmations"
+        "id":6,
+        "job_name":"template login",
+        "date_start":"12/11/2021",
+        "date_end":"15/11/2021",
+        "duration":"3 Days",
+        "price":"15 $",
+        "technologie":["html","css","jquery"],
+        "info":"template login using html and css with jquery the template should with script of validation of informations"
     }, 
     {
         
-        id:7,
-        jobname:"vedio animation",
-        datesatart:"12/11/2021",
-        datefin:"15/11/2021",
-        duration:"13 Days",
-        price:"10 $",
-        technologie:['filmora',"photoshop","english"],
-        info:"vedio animatio duration of vedio is 60 second "
+        "id":7,
+        "job_name":"vedio animation",
+        "date_start":"12/11/2021",
+        "date_end":"15/11/2021",
+        "duration":"13 Days",
+        "price":"10 $",
+        "technologie":["filmora","photoshop","english"],
+        "info":"vedio animatio duration of vedio is 60 second "
     }, 
     {
         
-        id:8,
-        jobname:"phtoshop pictures",
-        datesatart:"12/11/2021",
-        datefin:"20/11/2021",
-        duration:"8 Days",
-        price:"30 $",
-        technologie:['html',"css","jquery"],
-        info:"photoshop of several picture 15 picture should make change in background and color for more information contact us"
+        "id":8,
+        "job_name":"phtoshop pictures",
+        "date_start":"12/11/2021",
+        "date_end":"20/11/2021",
+        "duration":"8 Days",
+        "price":"30 $",
+        "technologie":["html","css","jquery"],
+        "info":"photoshop of several picture 15 picture should make change in background and color for more informations contact us"
     }, 
 ]
 let Company = [
   {
-    id:1,
-    name:'aserti',
-    avatar:"",
-    email:"asert@email.com",
-    description:"La société ASERTI est un acteur incontournable sur les marchés Composée d'une équipe spécialisée dans l'industrie agro-alimentaire, ASERTI propose à ses clients la conception,"
+    "id":1,
+    "name":"aserti",
+    "avatar":"",
+    "email":"asert@email.com",
+    "description":"La société ASERTI est un acteur incontournable sur les marchés Composée dune équipe spécialisée dans l industrie agro-alimentaire, ASERTI propose à ses clients la conception,"
   },
   {
-    id:2,
-    name:'Atlas',
-    avatar:"",
-    email:"atlasmaroc@email.com",
-    description:"ATLAS est un acteur incontournable sur les marchés Composée d'une équipe spécialisée dans l'industrie agro-alimentaire, ASERTI propose à ses clients la conception,"
+    "id":2,
+    "name":"Atlas",
+    "avatar":"",
+    "email":"atlasmaroc@email.com",
+    "description":"ATLAS est un acteur incontournable sur les marchés Composée dune équipe spécialisée dans lindustrie agro-alimentaire, ASERTI propose à ses clients la conception,"
   },
   {
-    id:3,
-    name:'Bim',
-    avatar:"",
-    email:"bim@aswak.com",
-    description:"Bim est un entreprise qui vend plusieurs types des produits eletronice informatique nouritaure..."
+    "id":3,
+    "name":"Bim",
+    "avatar":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu01Z6d-ako7ocBnzO4XJJnbqUmjRYSWIF_iU9Mvu0mcFAtsCdK4NuKe_22vCx1LllrK0&usqp=CAU",
+    "email":"bim@aswak.com",
+    "description":"Bim est un entreprise qui vend plusieurs types des produits eletronice infomatique nouritaure..."
   },
   {
-    id:4,
-    name:'asima',
-    avatar:"",
-    email:"micrpspft@email.com",
-    description:"asimsa ou aswak salam est un entreprise qui spesiale en vendre des produits ,"
+    "id":4,
+    "name":"asima",
+    "avatar":"https://wajaheni.com/wp-content/uploads/2020/08/ACIMA.png",
+    "email":"micrpspft@email.com",
+    "description":"asimsa ou aswak salam est un entreprise qui spesiale en vendre des produits ,"
   },
   {
-    id:5,
-    name:'netplus',
-    avatar:"",
-    email:"asert@email.com",
-    description:"La société NETPLUS est soiete de security et d'analyses les sites web la societe a un equipe spécialisée dans l'infrmatique la societe propose à ses clients de proteeger son site avec une grand perfomance"
+    "id":5,
+    "name":"netplus",
+    "avatar":"https://i.pinimg.com/280x280_RS/0d/d4/e0/0dd4e043ad08f6812f3e6c496a78defa.jpg",
+    "email":"asert@email.com",
+    "description":"La société NETPLUS est soiete de security et danalyses les sites web la societe a un equipe spécialisée dans l infrmatique la societe propose à ses clients de proteeger son site avec une grand perfomance"
+  },
+  {
+    "id":6,
+    "name":"Attijariwafa bank",
+    "avatar":"https://www.attijariwafabank.com/sites/default/files/2020-09/logo_awb.png",
+    "email":"Attijariwafabank@bank.com",
+    "description":"Attijariwafa bank est un groupe bancaire marocain. Première banque du Maroc, d Afrique du nord et 4ᵉ banque d Afrique, elle compte près de 10 millions de clients particuliers, professionnels, entreprises & institutionnels et 20 602 employés sur 25 pays."
   }
 ]
